c9a49f3e6dbf69af4eb0aeb8903d14371f18a399
jstreight-digital <streightdigital@gmail.com>
2026-09-15 14:37:02 -0700

    Connect API to Square environment variable

--- api/products.js
+++ api/products.js
@@ -1,5 +1,13 @@
 export default async function handler(req, res) {
+  const token = process.env.SQUARE_ACCESS_TOKEN;
+
+  if (!token) {
+    return res.status(500).json({
+      error: "Square access token is not configured"
+    });
+  }
+
   res.status(200).json({
-    message: "Square API endpoint is working!"
+    message: "Square access token is available!"
   });
 }
