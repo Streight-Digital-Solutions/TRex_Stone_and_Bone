47132d3b518cf74993014aa22ae509492e173807
jstreight-digital <streightdigital@gmail.com>
2026-09-15 15:26:15 -0700

    Return Square catalog as JSON

--- api/products.js
+++ api/products.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
       return res.status(response.status).json(data);
     }
 
-    return res.status(200).send(data);
+    return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({
       error: "Failed to connect to Square",
