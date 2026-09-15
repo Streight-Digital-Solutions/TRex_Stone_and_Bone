6b758b0d2468a6a0a72480ac708efc7d1db9490e
jstreight-digital <streightdigital@gmail.com>
2026-09-15 15:27:53 -0700

    Disable API response caching

--- api/products.js
+++ api/products.js
@@ -25,6 +25,7 @@ export default async function handler(req, res) {
       return res.status(response.status).json(data);
     }
 
+    res.setHeader("Cache-Control", "no-store");
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({
